e54bfb68026258ce20907d5a520d66cef60f5d86
David Tumpowsky <tumpowskydavid@gmail.com>
2018-04-06 15:13:44 -0600

    click functioning on ul

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,23 @@
 //Backend
+function Customer (first, last) {
+  this.firstName = first;
+  this.lastName = last;
+}
+
+
+
 function Pizza (crust, sauce, toppings) {
   this.crust = crust
   this.sauce = sauce
   this.toppings = toppings
 }
 
+function Contact(first, last) {
+  this.firstName = first;
+  this.lastName = last;
+}
+
+var toppingArr=[]
 
 
 // Pizza.prototype.this = function() {
@@ -14,9 +27,29 @@ function Pizza (crust, sauce, toppings) {
 
 //Front End
 $(document).ready(function() {
-  $("#toppings").click(function() {
-    console.log(this)
+  $("#new-order").submit(function() {
+    event.preventDefault();
+
+  var inputtedFirstName = $("input#new-first-name").val();
+  var inputtedLastName = $("input#new-last-name").val();
+
+  var patron = new Customer(inputtedFirstName, inputtedLastName);
+
+  console.log(patron)
+
   });
+
+  $("ul.topping-list").click(function() {
+    var choice = $(this).attr('id')
+
+    $(toppingArr.push(choice));
+
+  console.log(toppingArr)
+console.log(choice)
+
+  });
+
+
 
 
 
